f64d4cdb62721d4903e20eb7d01e91fa72dc9c19
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-11-15 13:15:21 +0200

    Add menu items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Menu from './Components/Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      	<Menu />
       </div>
     );
   }
---
+++ src/Components/Menu.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  menuItem: {
+    color: '#fff'
+  }
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <MenuItem classes={{root: classes.menuItem}}>Я подвезу</MenuItem>
+          <MenuItem classes={{root: classes.menuItem}}>Меня подвезите</MenuItem>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
