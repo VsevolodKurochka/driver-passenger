321ff5f2e09c54618c246dceba50f645d2e466d3
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-11-15 19:17:22 +0200

    Update driver component and simple card

--- src/partials/SimpleCard.js
+++ src/partials/SimpleCard.js
@@ -15,31 +15,40 @@ const styles = {
   title: {
     fontSize: 14,
   },
+  subtitle: {
+    lineHeight: 1.2,
+    marginBottom: 15
+  },
   pos: {
+    marginBottom: 12,
+  },
+  phone: {
     marginBottom: 12,
   },
 }
 
 function SimpleCard(props) {
   const { classes } = props
+  const phone = props.data.phone
+  const details = props.data.details
 
   return (
+    
+
     <Col sm="6" md="4" className="item-column">
       <Card className={classes.card}>
         <CardContent>
           <Typography className={classes.title} color="textSecondary" gutterBottom>
             Имя: {props.data.name}
           </Typography>
-          <Typography variant="h5" component="h2">
+          <Typography variant="h6" component="h2" className={classes.subtitle}>
             {props.data.start} - {props.data.end}
           </Typography>
           <Typography className={classes.pos} color="textSecondary">
             Дата: {props.data.time}
           </Typography>
-          <Typography component="p">
-            Примечания: {props.data.details}
-            <br/>
-          </Typography>
+          {phone ? <Typography className={classes.phone} color="textSecondary">Телефон: <a href={"tel:" + props.data.phone}>{props.data.phone}</a></Typography> : ""}
+          {details ? <Typography component="p">Примечания: {props.data.details}</Typography> : ""}
         </CardContent>
       </Card>
     </Col>
