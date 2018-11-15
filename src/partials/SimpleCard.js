cd1604433ff4b2fbeaa0ae82a3716c9ef24cbd4d
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-11-15 18:21:34 +0200

    Update SimpleCard.js

--- src/partials/SimpleCard.js
+++ src/partials/SimpleCard.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
 // Bootstrap
-import { Col } from 'reactstrap';
+import { Col } from 'reactstrap'
 
 const styles = {
   card: {
     minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
   },
   title: {
     fontSize: 14,
@@ -25,11 +19,10 @@ const styles = {
   pos: {
     marginBottom: 12,
   },
-};
+}
 
 function SimpleCard(props) {
-  const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
+  const { classes } = props
 
   return (
     <Col sm="6" md="4">
@@ -51,11 +44,11 @@ function SimpleCard(props) {
         </CardContent>
       </Card>
     </Col>
-  );
+  )
 }
 
 SimpleCard.propTypes = {
   classes: PropTypes.object.isRequired,
-};
+}
 
-export default withStyles(styles)(SimpleCard);
+export default withStyles(styles)(SimpleCard)
