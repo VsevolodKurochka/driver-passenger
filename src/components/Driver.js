113bda52455a1ca3281675a09ea69167405d7a8a
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-11-15 15:19:00 +0200

    Update driver component

--- src/components/Driver.js
+++ src/components/Driver.js
@@ -1,11 +1,128 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
-export default class Driver extends Component {
+// Firebase
+import * as firebase from 'firebase'
+
+// Material
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class Driver extends Component {
+	constructor(){
+		super();
+		this.state = {
+			name: '',
+			start: '',
+			end: '',
+			time: '',
+			details: '',
+			speed: 15
+		};
+	}
+
+	handleForm = event => {
+		event.preventDefault();
+	}
+	
+	handleChange = event => {
+		
+		const name = event.target.name;
+		const value = event.target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+	componentDidMount(){
+		const rootRef = firebase.database().ref().child('drivers');
+		const speedRef = rootRef.child('speed');
+		speedRef.on('value', snap => {
+			this.setState({
+				speed: snap.val()
+			});
+		});
+	}
   render() {
+  	const { classes } = this.props;
     return (
-      <div className='row'>
-        <div className='col-md-12'>Раздел /Водитель</div>
-      </div>
+      <section className='section'>
+        <div className="container">
+        	<Typography component="h1" variant="h3">#Подвезу</Typography>
+        	<Typography component="h2" variant="h6" gutterBottom>Заполните форму, чтобы добавить объявление о поездке</Typography>
+		      <form onSubmit={this.handleForm} noValidate autoComplete="off">
+		      	<TextField
+		          label="Имя и фамилия"
+		          value={this.state.name}
+		          name="name"
+		          onChange={this.handleChange}
+		          margin="normal"
+		          className={classes.textField}
+		          variant="outlined"
+		        />
+		        <TextField
+		          label="Откуда"
+		          value={this.state.start}
+		          name="name"
+		          onChange={this.handleChange}
+		          margin="normal"
+		          className={classes.textField}
+		          variant="outlined"
+		        />
+		        <TextField
+		          label="Куда"
+		          value={this.state.end}
+		          name="name"
+		          onChange={this.handleChange}
+		          margin="normal"
+		          className={classes.textField}
+		          variant="outlined"
+		        />
+		        <TextField
+		          label="Время"
+		          value={this.state.time}
+		          name="name"
+		          onChange={this.handleChange}
+		          margin="normal"
+		          className={classes.textField}
+		          variant="outlined"
+		        />
+		        <TextField
+		          label="Примечания"
+		          value={this.state.details}
+		          name="name"
+		          onChange={this.handleChange}
+		          margin="normal"
+		          className={classes.textField}
+		          variant="outlined"
+		        />
+		        <Button type="submit" variant="contained" size="large" color="primary" className={classes.button}>
+			        Заполнить форму
+			      </Button>
+		      </form>
+        </div>
+      </section>
     )
   }
 }
+
+Driver.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(Driver);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // General
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './index.css'
 
 // Partials
@@ -10,6 +10,20 @@ import Menu from './partials/Menu'
 // Components
 import Driver from './components/Driver'
 import Passenger from './components/Passenger'
+
+// Firebase
+import * as firebase from 'firebase'
+
+var config = {
+  apiKey: "AIzaSyD19vjU4cTw_5Znaz2wYZRkYpEPfkIQbqo",
+  authDomain: "cherkassy-aa040.firebaseapp.com",
+  databaseURL: "https://cherkassy-aa040.firebaseio.com",
+  projectId: "cherkassy-aa040",
+  storageBucket: "",
+  messagingSenderId: "1011221614650"
+};
+firebase.initializeApp(config);
+
 
 ReactDOM.render(
 	<Router>
